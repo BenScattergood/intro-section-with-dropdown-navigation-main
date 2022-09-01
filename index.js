171430265fc4064b52e671d3699230243e25d49e
Ben Scattergood <ben.s1234@hotmail.co.uk>
2022-09-01 14:37:35 +0100

    added background filter and javascript

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const hamburgerDiv = document.querySelector("#hamburger-div");
 const navContents = document.querySelector("#nav-contents");
 const hamburger = document.querySelector("#hamburger");
 const exitBtn = document.querySelector("#exit-btn");
+const pageMask = document.querySelector("#page-mask");
+
+let dropDownTally = 0;
+let hamburgerTally = 0;
 
 for (let i = 0; i < dropDownHeaders.length; i++) {
+    DropDownEvent(i);
+}
+
+hamburgerDiv.addEventListener("click", () => {
+    HamburgerEvent();
+})
+
+function DropDownEvent(i) {
     dropDownHeaders[i].addEventListener("click", () => {
         dropDownPopups[i].classList.toggle("hidden");
 
@@ -15,8 +27,30 @@ for (let i = 0; i < dropDownHeaders.length; i++) {
     })
 }
 
-hamburgerDiv.addEventListener("click", () => {
+function HamburgerEvent() {
     navContents.classList.toggle("hidden");
     hamburger.classList.toggle("hidden");
     exitBtn.classList.toggle("hidden");
-})
+    pageMask.classList.toggle("hidden");
+    hamburgerTally++;
+}
+
+if (window.attachEvent) {
+    window.attachEvent('onresize', () => {
+        if (window.innerWidth > 800) {
+            if (hamburgerTally % 2 !== 0) {
+                HamburgerEvent();
+            }
+        }
+    });
+} else if (window.addEventListener) {
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 800) {
+            if (hamburgerTally % 2 !== 0) {
+                HamburgerEvent();
+            }
+        }
+    }, true);
+} else {
+    //The browser does not support Javascript event binding
+}
